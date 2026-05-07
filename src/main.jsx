bf77086bb4eb412c7f561fbe19ca952e1174625f
supprintdrill <supprintdrill@gmail.com>
2026-05-07 18:35:02 -0500

    Fix Vercel production asset path

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { HelmetProvider } from "react-helmet-async";
-
 import App from "./App.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </HelmetProvider>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
